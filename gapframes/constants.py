771413612cf50c02327a569cae35e31be2f09f8c
kalogane <kalogane@gmail.com>
2020-02-15 19:21:00 +0000

    split out some UI elements into another tab, added hotkey fields

--- gapframes/constants.py
+++ gapframes/constants.py
@@ -3,7 +3,7 @@ import os
 
 # Find the UI file in the "ui" submodule directory of gapframes.
 _, gapframes_path, _ = imp.find_module("gapframes")
-PANEL_UI_PATH =  os.path.join(gapframes_path, "ui", "GapframesPanel.ui")
+PANEL_UI_PATH = os.path.join(gapframes_path, "ui", "GapframesPanel.ui")
 
 # Do a stat check to make sure file can be accessed, error if not because we need it.
 try:
